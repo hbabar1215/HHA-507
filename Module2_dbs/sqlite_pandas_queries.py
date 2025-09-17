ab88b55ad929a7eceb791638b70f8d05e71bac49
hbabar1215 <humababar1010@gmail.com>
2025-09-17 19:03:24 -0400

    sql query

--- Module2_dbs/sqlite_pandas_queries.py
+++ Module2_dbs/sqlite_pandas_queries.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import gc # garbage collector
 
 ## load in .sql file as string:
-with open('Module2_dbs/patient_query.sql', 'r') as file:
+with open('HHA-507-2025\Module2_dbs\patient_query.sql', 'r') as file:
     sql_depression_query = file.read()
 
 
@@ -11,7 +11,7 @@ db_location = 'example.db'
 
 engine = create_engine(f'sqlite:///{db_location}')
 
-patients_df = pd.read_csv('assignments/assignment2_files/patients.csv')
+patients_df = pd.read_csv('HHA-507-2025/assignments/assignment2_files/patients.csv')
 
 patients_df.to_sql('patients_details', con=engine, if_exists='replace', index=False)
 
@@ -19,11 +19,11 @@ query_anxiety = "SELECT * FROM patients_details WHERE primary_icd10 = 'F41.9'"
 
 results_df = pd.read_sql_query(query_anxiety, con=engine)
 len(results_df)
-
+print(results_df)
 
 
 
 query_x = "SELECT * FROM patients_details WHERE primary_icd10 = 'M54.5'"
 results_x_df = pd.read_sql_query(query_x, con=engine)
 len(results_x_df)
-
+print(results_x_df)
